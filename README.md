ab2bec4bf61b5732e946092d37d6f59618daa792
quangngoc430 <ngoc.bui150019@vnuk.edu.vn>
2019-06-27 11:00:52 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# docker-nginx-express
+
+> A playground for Docker with Nginx and Express.
+
+# Development
+
+* Cloning the repo
+
+```bash
+$ git clone https://github.com/quangngoc430/docker-nginx-express.git
+```
+
+* Installing dependencies
+
+```bash
+$ npm install
+```
+
+# Docker
+
+* Building an image
+
+```bash
+$ docker-compose build
+```
+
+* Running containers
+
+```bash
+$ docker-compose up
+```
+
+* Stopping  containers
+
+```bash
+$ docker-compose down
+```
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const winston = require('winston');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(process.env.PORT, () => {
+  winston.info(`NODE_ENV: ${process.env.NODE_ENV}`);
+  winston.info(`INSTANCE: ${process.env.INSTANCE}`);
+  winston.info(`EXPRESS: ${process.env.PORT}`);
+});
